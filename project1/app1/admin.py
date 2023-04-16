7539b240e415fbb66519a2443acbe0654c7604d8
Nitin Swami <nitinswami95@gmail.com>
2023-04-16 22:22:19 +0530

    added models and admin file

--- project1/app1/admin.py
+++ project1/app1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Student_Model
 # Register your models here.
+
+@admin.register(Student_Model)
+class Student_Admin(admin.ModelAdmin):
+    model =  Student_Model
+    list_display = ['student_id', 'student_fname', 'student_lname', 'address', 'grade']
--- project1/app1/models.py
+++ project1/app1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student_Model(models.Model):
+    student_id = models.IntegerField(primary_key=True)
+    student_fname = models.CharField(max_length=30)
+    student_lname = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    grade = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.student_id}-{self.student_fname}'
+    
+    
